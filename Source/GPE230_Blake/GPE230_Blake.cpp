d7c3b207cef5c4767025861b2fb8b08bac67e1e9
ProfRatliff <bratliff@uat.edu>
2023-05-03 16:08:31 -0700

    Added Unreal project

---
+++ Source/GPE230_Blake/GPE230_Blake.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GPE230_Blake.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GPE230_Blake, "GPE230_Blake" );
---
+++ Source/GPE230_Blake/GPE230_BlakeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GPE230_BlakeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GPE230_BLAKE_API AGPE230_BlakeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
